fc7d0b37f8c6ff2a5337f64325212e0a126f678e
Patrick <amadifaraday@gmail.com>
2025-06-18 21:46:25 +0100

    feat: added a log

--- duumps/working/Notification.js
+++ duumps/working/Notification.js
@@ -14,6 +14,7 @@ dotenv.config();
 let client;
 let isInitialized = false;
 
+//console.log()
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
